b15197adb34967023d69f79a6bc6e982e3a628ff
Devansh Sharma <devansharma92@gmail.com>
2020-11-26 15:37:17 +0530

    Add files via upload

---
+++ Breast_Cancer API/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, url_for, flash, redirect
+import joblib
+from flask import request
+import numpy as np
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route("/")
+
+@app.route("/cancer")
+def cancer():
+    return render_template(r"C:\Users\Mahesh Sharma\Desktop\HealthApp\Indivisual_Deployment\Breast_Cancer API\cancer_model.pkl")
+
+def ValuePredictor(to_predict_list, size):
+    to_predict = np.array(to_predict_list).reshape(1,size)
+    if(size==5):
+        loaded_model = joblib.load('cancer_model.pkl')
+        result = loaded_model.predict(to_predict)
+    return result[0]
+
+@app.route('/predict', methods = ["POST"])
+def predict():
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+         #cancer
+        if(len(to_predict_list)==5):
+            result = ValuePredictor(to_predict_list,5)
+    
+    if(int(result)==1):
+        prediction = "Sorry you chances of getting the disease. Please consult the doctor immediately"
+    else:
+        prediction = "No need to fear. You have no dangerous symptoms of the disease"
+    return(render_template("result.html", prediction_text=prediction))       
+
+if __name__ == "__main__":
+    app.run(debug=True)
